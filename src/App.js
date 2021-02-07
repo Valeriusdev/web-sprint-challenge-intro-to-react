0b47a14d524fb41478970e7b75a795d0ee0bfc78
Valeriusdev <kakao757@gmail.com>
2021-02-07 14:52:38 -0800

    used styled components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -8,10 +10,21 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  
+  const [data, setData] = useState ([]);
+  useEffect(() =>{
+  axios  
+  .get('https://swapi.dev/api/people')
+  .then(res => {  
+    console.log(res.data)  
+    setData(res.data);
+  });
+},[]);
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character mydata = {data}/>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,82 @@
 // Write your Character component here
+
+import React from 'react';
+// import {Card, CardTitle, Container, Row, Col, CardSubtitle} from 'reactstrap';
+import styled from "styled-components";
+
+
+// using styled components
+
+const MyCard = styled.section`
+width: 25%;
+border: 15px solid blue;
+padding: 20px;
+margin: 20px;
+display: flex;
+flex-direction: column;
+alight-items: center;
+justify-content: space-between;
+background: #0d7028;
+`;
+
+const Title = styled.h1`
+font-size: 2 em;
+color: aquamarine;
+`
+
+const Paragraph = styled.div`
+font-size: 1.5 em;
+color: chocolate; 
+`
+
+const MyContainer = styled.div`
+border: 5px solid navy;
+`
+
+
+function Character(props){    
+    return(
+        <MyContainer>
+            {props.mydata.map((element) => {
+               return(                   
+                   
+                <MyCard>
+                    <Title>{element.name} </Title>
+                    <Paragraph> Height: {element.height} </Paragraph>
+                    <Paragraph> Mass: {element.mass} </Paragraph>
+                    <Paragraph> Hair Color: {element.hair_color}</Paragraph>
+                </MyCard>
+                    
+                );
+            })}
+        </MyContainer>
+    );
+};
+
+export default Character;
+
+
+// using bootstrap
+//
+// function Character(props){    
+//     return(
+//         <Container>
+//             <Row>
+//             {props.mydata.map((element) => {
+//                return(                   
+//                    <Col sm ='2'>
+//                    <Card key = {element.created} style ={{backgroundColor: '#0d7028'}}>
+//                         <CardTitle tag="h4">{element.name}</CardTitle>
+//                         <CardSubtitle tag="h5"> Height: {element.height} </CardSubtitle>
+//                         <CardSubtitle tag="h5"> Mass: {element.mass} </CardSubtitle>
+//                         <CardSubtitle tag="h6"> Hair Color: {element.hair_color}</CardSubtitle>                        
+//                     </Card>
+//                    </Col>                   
+//                 );
+//             })}
+//             </Row>            
+//         </Container>
+//     );
+// };
+
+// export default Character;
